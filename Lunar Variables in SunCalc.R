df16b2a870e7bb28f8d373567d9eecc7b18f44ae
mscofield-mpg <129301894+mscofield-mpg@users.noreply.github.com>
2023-11-10 14:12:30 -0700

    Update Lunar Variables in SunCalc.R

--- Lunar Variables in SunCalc.R
+++ Lunar Variables in SunCalc.R
@@ -66,3 +66,63 @@ left_join(sun.dat, by ="date")
 #save dataframes as csv just in case
 write.csv(moon.dat, file = "./Moonlight_hourly_DATES.csv", row.names = FALSE)
 write.csv(moon.time, file = "./Moon_daily_time_DATES.csv", row.names = FALSE)
+
+
+
+###Lunar Functions###
+
+Date.2020<-as.Date(as.Date("2020-08-15"):as.Date("2020-12-31"), origin = "1970-01-01")
+
+moon.time.2020<-getMoonTimes(date = Date.2020, lat = 46.7, lon = -113.9, data = NULL, keep = c("rise",
+                                                                                               "set"),
+                             tz = "UTC",
+                             inUTC = TRUE)
+sun.time.2020<-getSunlightTimes(date = Date.2020, 
+                                lat = 46.7,
+                                lon = -113.9,
+                                data = NULL,
+                                keep = c("sunset", "dusk", "dawn", "sunrise"),
+                                tz = "UTC")
+Night.dat<-moon.time.2020 %>%
+  left_join(sun.time.2020, by = "date")
+
+Night.dat<-rename(Night.dat, moonrise = "rise", moonset = "set")
+Night.dat.sel<-Night.dat%>%
+  select("date", "moonrise", "moonset", "sunset", "dusk", "dawn", "sunrise")
+
+# Create a function to check if the moon is available during the night
+moon_available <- function(night_start, night_end, moonrise, moonset) {
+  # Check if the moon is up at the start of the night or rises during the night
+  moon_up_at_start <- !is.na(moonrise) && moonrise <= night_start && (is.na(moonset) || moonset >= night_start)
+  
+  # Check if the moon is up at any point during the night
+  moon_up_during_night <- !is.na(moonrise) && moonrise >= night_start && moonrise <= night_end
+  
+  # Check if the moon is still up at the end of the night or sets during the night
+  moon_up_at_end <- !is.na(moonset) && moonset <= night_end && (is.na(moonrise) || moonrise <= night_end)
+  
+  # The moon is available if any of the above conditions are met
+  return(moon_up_at_start || moon_up_during_night || moon_up_at_end)
+}
+
+
+# Create a function to assign night IDs and moon availability
+create_night_variables <- function(row) {
+  dusk <- row$dusk
+  dawn <- row$dawn
+  
+  # Assign a unique night ID based on the date
+  night_id <- as.numeric(difftime(row$date, min(Night.dat.sel$date), units = "days")) + 1
+  
+  # Check if the moon is available during the night
+  moon_avail <- moon_available(dusk, dawn, row$moonrise, row$moonset)
+  
+  return(data.frame(Night_ID = night_id, Moon_Availability = moon_avail))
+}
+
+# Apply the function to each row of the data frame
+Night.dat.new <- Night.dat.sel %>%
+  rowwise() %>%
+  do(create_night_variables(.))
+
+Night.dat.sel<-cbind(Night.dat.sel, Night_ID = Night.dat.new$Night_ID, Moon_Availability = Night.dat.new$Moon_Availability)
