db5f2ed00178989d5cf4bf19199dac7bdd343cfd
mscofield-mpg <129301894+mscofield-mpg@users.noreply.github.com>
2024-01-10 14:20:06 -0700

    Update Lunar Variables in SunCalc.R
    Updated functions with simple script for calling data frames of lunar variables for each time_bin for each year

--- Lunar Variables in SunCalc.R
+++ Lunar Variables in SunCalc.R
@@ -1,4 +1,176 @@
+###### CALLING LUNAR VARIABLES AND JOINING NUMERIC TABLES#######
 
+
+#######Load Packages##########
+library(suncalc)
+library(dplyr)
+library(lubridate)
+
+#######LOAD FUNCTIONS###########
+
+#pull moon rise/set times and sun rise/set times
+get_moon_times<- function(Date) {
+  
+  moon.time<-getMoonTimes(date = Date, 
+                          lat = 46.7, lon = -113.9, 
+                          data = NULL, 
+                          keep = c("rise", "set"),
+                          tz = "America/Denver",
+                          inUTC = FALSE)
+  
+  sun.time<-getSunlightTimes(date = Date, 
+                             lat = 46.7,
+                             lon = -113.9,
+                             data = NULL,
+                             keep = c("sunset", "dusk","nauticalDusk", "nauticalDawn", 
+                                      "dawn", "sunrise"),
+                             tz = "America/Denver")
+  
+  Night.dat<-moon.time %>%
+    left_join(sun.time, by = "date")
+  
+  
+  Night.dat<-rename(Night.dat, moonrise = "rise", moonset = "set")
+  
+  return(Night.dat)
+}
+
+#pull moon illumination and position variables and join with moon/sun time variables
+get_moon_variables <- function(Date.time) {
+  
+  moon.ill <- getMoonIllumination(date = Date.time, keep = c("fraction", "phase", "angle"))
+  moon.pos <- getMoonPosition(date = Date.time, lat = 46.7, lon = -113.9, data = NULL, keep = c("altitude", "azimuth", "parallacticAngle"))
+  
+  moon.dat <- moon.ill %>%
+    left_join(moon.pos, by = "date") %>%
+    mutate(time_bin = date) %>%
+    select(-date, -lat, -lon)
+  
+  moon.dat <- moon.dat %>%
+    mutate(time_bin = force_tz(time_bin, tz = "America/Denver")) %>%
+    left_join(Night.dat, join_by(closest(time_bin >= sunset))) %>%
+    filter(!is.na(Night_ID))
+  
+  moon.dat <- moon.dat %>%
+    arrange(Night_ID, time_bin) %>%
+    group_by(Night_ID) %>%
+    mutate(tb_position = row_number() - 1) %>%
+    ungroup()
+  
+  moon.dat <- moon.dat %>%
+    select(time_bin, Night_ID, tb_position, everything()) %>%
+    select(-date)
+  
+  return(moon.dat)
+}
+
+#calculate moon availability and moon duration
+moon_info <- function(df, time_bin_duration_minutes) {
+  # Initialize columns
+  df$moon_available <- 0
+  df$moon_duration <- 0
+  
+  # Loop through each row in the data frame
+  for (i in 1:nrow(df)) {
+    moonrise_time <- df$moonrise[i]
+    moonset_time <- df$moonset[i]
+    time_bin_start <- df$time_bin[i]
+    time_bin_end <- time_bin_start + (time_bin_duration_minutes * 60)
+    
+    # Check if moon is available during the time_bin
+    if (!is.na(moonrise_time) && !is.na(moonset_time)) {
+      if ((time_bin_start >= moonrise_time && time_bin_end <= moonset_time) ||
+          (time_bin_start <= moonrise_time && time_bin_end >= moonrise_time) ||
+          (time_bin_start <= moonset_time && time_bin_end >= moonset_time)) {
+        df$moon_available[i] <- 1
+      }
+      
+      # Calculate moon duration within the time_bin
+      start_time <- max(time_bin_start, moonrise_time)
+      end_time <- min(time_bin_end, moonset_time)
+      duration_minutes <- as.numeric(difftime(end_time, start_time, units = "mins"))
+      duration_minutes <- max(0, duration_minutes)
+      
+      # Assign the calculated duration to the moon_duration column
+      df$moon_duration[i] <- duration_minutes
+    }
+  }
+  
+  return(df)
+}
+
+
+######## NIGHT DATA MANIPULATION#########
+
+#set Date factor used in get_moon_times function
+Date<-as.Date(as.Date("2023-08-15"):as.Date("2024-01-02"), origin = "1970-01-01")
+
+#function call
+Night.dat<-get_moon_times(Date)
+
+#save as csv
+write.csv(Night.dat, "./Lunar/Moon Data/2023/Moon Times 2023.csv", row.names = FALSE)
+
+#Pulled into excel, minor manipulations I haven't figured out how to do in R yet
+  #added Night_ID
+  #adjusted sunrise dates to align with previous sunset (Night_start = sunset, Night_end = following sunrise)
+  #fixed moonrise/moonset times to align with start_time as sunset instead of midnight
+#save as csv
+
+#Read in adjusted Night variables after Excel modifications
+Night.dat<- read_csv("./Lunar/Moon Data/2023/Moon Times 2023.csv")
+
+#reformat date time variables
+Night.dat <- Night.dat %>%
+  mutate(across(c(moonrise, moonset, sunset, dusk, nauticalDusk, nauticalDawn, dawn, sunrise),
+                ~ as.POSIXct(trimws(.), format = "%m/%d/%y %H:%M"))) 
+
+Night.dat$date<-as.Date(Night.dat$date, format = "%m/%d/%y")
+
+#calculate length of "Night" in minutes (dusk-dawn)
+Night.dat<- Night.dat %>%
+  mutate(night_mins = abs(as.numeric(difftime(dusk, dawn, tz = "America/Denver", units = "mins"))))
+
+############ LUNAR ILLUMINATION AND POSITION VARIABLES ############
+
+#set Date.time vector used in get_moon_variables function
+  #be sure to adjust the time bin as needed
+Date.time <- seq(ISOdate(2023, 8, 15), ISOdate(2024, 01, 01), "15 mins")
+
+#call function
+moon.dat <- get_moon_variables(Date.time)
+
+#call second function
+  #be sure to adjust time_bin_duration to match the Date.time vector
+moon.dat <- moon_info(moon.dat, time_bin_duration_minutes = 30)
+
+#add time_bin_ID (numeric)
+moon.dat<- moon.dat %>%
+  arrange(time_bin) %>%
+  mutate(tb_ID = row_number()) 
+
+#select time_bin and numeric fields for future joining with activity/detection table
+moon.numeric<- moon.dat %>%
+  select(Night_ID, time_bin, tb_ID, tb_position, night_mins, moon_available, moon_duration, fraction, phase, angle,
+         altitude, azimuth, parallacticAngle)
+
+#save both tables as csv
+write.csv(moon.dat, "./Lunar/Moon Data/2023/30min_Moon Variables.csv", row.names = FALSE)
+write.csv(moon.numeric, "./Lunar/Moon Data/2023/30min_Numeric_Moon Variables.csv", row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+####OUTDATED VERSIONS BELOW#####
 ##Pulling lunar variables using SunCalc package##
 
 setwd()
@@ -152,7 +324,7 @@ write.csv(moon.numeric, "./Lunar/Moon Data/2020/5min_Numeric_Moon Variables.csv"
 
 
 
-####OUTDATED VERSIONS BELOW#####
+
 #Function for illumination and position variables
 #Change time_bin (5 mins, 10 mins, 15 mins, 30 mins, hours etc) as needed in Date.time vector
 get_moon_variables <- function() {
