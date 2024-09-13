b5aff02294533a35cf59ded61d642585cdf8f604
mscofield-mpg <129301894+mscofield-mpg@users.noreply.github.com>
2024-09-13 11:56:27 -0600

    Update Common Poorwill Activity States.R
    Updated function definition, directories, and plotting lines. Added lunar variables at end.

--- Common Poorwill/Activity/Common Poorwill Activity States.R
+++ Common Poorwill/Activity/Common Poorwill Activity States.R
@@ -2,9 +2,265 @@
   #Created 6.18.2024#
   #Updated 8.7.2024#
   #Updated 8.29.24#
+  #Updated 9.13.24#
 library(tidyverse)
 library(viridis)
+library(fuzzyjoin)
 
+####Prepare Detection Data####
+#Load detections 
+COPO.detect<-read_csv("~/Library/CloudStorage/Egnyte-mpgcloud/Shared/Workspace/Teams/Avian Science/Motus/Project 213 Data/Processed Detection Data/COPO/8.31.2024/COPO_tag_detections_FILTERED_8.31.24.csv")
+
+#Convert to POSIXct format (the timestamps are in UTC, so we keep in UTC)
+COPO.detect$ts <- as.POSIXct(COPO.detect$ts, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+COPO.detect$tagDeployStart<- as.POSIXct(COPO.detect$tagDeployStart, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+
+#then force tz to Mountain time
+#modify time stamps and reduce variables
+COPO.detect<- COPO.detect %>%
+  mutate(ts_LocalTime = with_tz(ts, "America/Denver"),
+         date_Local = as.Date(format(ts_LocalTime, tz = "America/Denver")),
+         tagDeployStart_Local = with_tz(tagDeployStart, "America/Denver"))
+
+COPO.detect<- COPO.detect %>%
+  filter(motusFilter == 1)
+
+COPO.detect <- COPO.detect %>%
+  select(runID, motusTagID, mfgID, ts_LocalTime, date_Local, sig, port, recvName,
+         freqsd, runLen, noise, antType, antBearing, antHeight, recvDeployLat, recvDeployLon) %>%
+  arrange(motusTagID, ts_LocalTime) %>%
+  distinct(motusTagID, ts_LocalTime, recvName, port, .keep_all = TRUE)
+
+
+#filter detections to time period of interest, and combine tags that
+  #represent the same bird through the season
+COPO.act<- COPO.detect %>%
+  filter(ts_LocalTime >= "2024-05-01 00:00:00") %>%
+  mutate(mfgID = case_when(
+    mfgID == 148 ~ 598,
+    mfgID == 275 ~ 589,
+    mfgID == 154 ~ 586,
+    TRUE ~ as.numeric(mfgID)
+  ))
+####
+
+####Import Night Data (temporal)####
+Night.dat <- read_csv("~/Library/CloudStorage/Egnyte-mpgcloud/Shared/Workspace/Teams/Avian Science/Motus/Project 213 Data/Projects/COPO_Lunar Breeding Season/Moon Times 2024.csv", 
+                            col_types = cols(Night_ID = col_number(), 
+                                             Night_Start = col_datetime(format = "%m/%d/%y %H:%M"), 
+                                             Night_End = col_datetime(format = "%m/%d/%y %H:%M"), 
+                                             date = col_date(format = "%m/%d/%y"), 
+                                             moonrise = col_datetime(format = "%m/%d/%y %H:%M"),
+                                             moonset = col_datetime(format = "%m/%d/%y %H:%M"), 
+                                             sunset = col_datetime(format = "%m/%d/%y %H:%M"), 
+                                             dusk = col_datetime(format = "%m/%d/%y %H:%M"), 
+                                             nauticalDusk = col_datetime(format = "%m/%d/%y %H:%M"), 
+                                             nauticalDawn = col_datetime(format = "%m/%d/%y %H:%M"), 
+                                             dawn = col_datetime(format = "%m/%d/%y %H:%M"), 
+                                             sunrise = col_datetime(format = "%m/%d/%y %H:%M")))
+#Prepare Data for joining with Detections
+Night.dat <- Night.dat %>%
+  mutate(
+    Night_Start = force_tz(Night_Start, "America/Denver"),
+    Night_End = force_tz(Night_End, "America/Denver"),
+    moonrise = force_tz(moonrise, "America/Denver"),
+    moonset = force_tz(moonset, "America/Denver"),
+    sunset = force_tz(sunset, "America/Denver"),
+    dusk = force_tz(dusk, "America/Denver"),
+    nauticalDusk = force_tz(nauticalDusk, "America/Denver"),
+    nauticalDawn = force_tz(nauticalDawn, "America/Denver"),
+    dawn = force_tz(dawn, "America/Denver"),
+    sunrise = force_tz(sunrise, "America/Denver"))
+
+#calculate the length of Night Temp in minutes
+Night.dat<- Night.dat %>%
+  mutate(night_mins = abs(as.numeric(difftime(sunset, sunrise, tz = "America/Denver", units = "mins"))))
+####
+
+####Define Activity_Threshold####
+  #Modify as needed
+Activity_Threshold <- function(sd_sig) {
+  case_when(
+    sd_sig <= 2 ~ "Inactive",
+    sd_sig > 2 ~ "Active"
+  )
+}
+
+####
+
+####Define Activity_Function####
+  #Modify bin length to desired time in minutes
+Activity_Function <- function(COPO.act, Bin_Length = "1 min", Activity_Threshold, Night.dat) {
+  # Step 1: Process Activity Data
+  activity_table <- COPO.act %>%
+    mutate(time_bin = floor_date(ts_LocalTime, unit = Bin_Length)) %>%
+    group_by(mfgID, time_bin, recvName, port) %>%
+    mutate(sd_sig = sd(sig),
+           avg_sig = mean(sig),
+           num_recs = n()) %>%
+    ungroup() %>%
+    select(mfgID, date_Local, time_bin, sd_sig, avg_sig, port, recvName, antBearing, num_recs) %>%
+    distinct() %>%
+    group_by(mfgID, time_bin, recvName, port) %>%
+    mutate(State = Activity_Threshold(sd_sig)) %>%
+    ungroup()
+  
+  # Step 2: Join with Moon.dat
+  activity_table <- activity_table %>%
+    fuzzy_left_join(Night.dat,
+                    by = c("time_bin" = "Night_Start", "time_bin" = "Night_End"),
+                    match_fun = list(`>=`, `<=`)) %>%
+    select(mfgID, Night_ID, time_bin, everything(), -date_Local, -date, -Night_Start, -Night_End)
+  
+  # Step 3: Assign Day/Night Status
+  activity_table <- activity_table %>%
+    mutate(Temp = case_when(
+      (time_bin >= sunset  & time_bin < sunrise ) ~ "Night",
+      (time_bin >= sunrise  | time_bin < sunset ) ~ "Day"
+    )) %>%
+    distinct()
+  
+  #summary table
+  activity_table <- activity_table %>%
+    group_by(mfgID, time_bin) %>%
+    filter(avg_sig == max(avg_sig)) %>%
+    ungroup() %>%
+    filter(!is.na(State))
+  
+  bin_duration <- as.numeric(sub(" min", "", Bin_Length))
+  
+  activity_sum <- activity_table %>%
+    group_by(mfgID, Night_ID, Temp, State) %>%
+    summarise(
+      Total_Minutes = n() *  bin_duration
+    ) %>%
+    ungroup() %>%
+    pivot_wider(names_from = State, values_from = Total_Minutes, values_fill = 0)
+  
+  return(list(activity_table = activity_table, activity_sum = activity_sum))
+}
+
+#Run the function
+results <- Activity_Function(COPO.act, Bin_Length = "1 min", Activity_Threshold, Night.dat)
+
+#Extract results
+Activity_Table.1 <- results$activity_table
+Activity_Summary.1 <- results$activity_sum
+
+#Save data tables
+write_csv(Activity_Table.5b, "~/Library/CloudStorage/Egnyte-mpgcloud/Shared/Workspace/Teams/Avian Science/Motus/Project 213 Data/Projects/COPO_Lunar Breeding Season/Data Tables/Activity_Table_5mins.csv")
+write_csv(Activity_Summary.5, "~/Library/CloudStorage/Egnyte-mpgcloud/Shared/Workspace/Teams/Avian Science/Motus/Project 213 Data/Projects/COPO_Lunar Breeding Season/Data Tables/Activity_Summary_5mins.csv")
+####
+
+
+####Visualize#####
+
+###PLOT 1###
+  #this visualizes night time activity and day time activity by night for each tag
+  #Use the summary from above with desired time-bin
+#create vector
+mfg_ids <- unique(Activity_Summary.1$mfgID)
+
+#Loop through vector and save each plot
+for (ID in mfg_ids) {
+  plot_data <- filter(Activity_Summary.1, mfgID == ID)
+  
+  p <- ggplot(plot_data, aes(x = Night_ID)) +
+    geom_line(aes(y = Active, color = Temp, group = Temp), size = 1.2) +
+    geom_point(aes(y = Active, color = Temp), size = 3) +
+    labs(
+      title = paste("Minutes Active per Night COPO:", ID),
+      subtitle = "Bin Length = 1 min",
+      x = "Night ID",
+      y = "Minutes Active",
+      color = "Time Period"
+    ) +
+    scale_color_manual(values = c("Day" = "orange", "Night" = "blue")) +
+    theme_bw() +
+    theme(text = element_text(family = "serif", size = 12))
+
+  
+  ggsave(filename = paste0("Minutes Active by Night_COPO ", ID, ".png"), 
+         path = "~/Library/CloudStorage/Egnyte-mpgcloud/Shared/Workspace/Teams/Avian Science/Motus/Project 213 Data/Projects/COPO_Lunar Breeding Season/Figures/8.28.24/1 min", 
+         width = 7, 
+         height = 5, 
+         units = "in")
+  
+}
+
+####
+
+####Create Overview####
+Activity_Overview<- Activity_Summary.10 %>%
+  mutate(Bin_Length = 10)
+
+Activity_Summary.8b<- Activity_Summary.8 %>%
+  mutate(Bin_Length = 8)
+
+Activity_Overview<- Activity_Overview %>%
+  bind_rows(Activity_Summary.9b, Activity_Summary.8b, Activity_Summary.7b, Activity_Summary.6b,
+            Activity_Summary.5b, Activity_Summary.4b, Activity_Summary.3b, Activity_Summary.2b,
+            Activity_Summary.1b)
+
+#Let's just look at nighttime activity
+Activity_Overview.sht<- Activity_Overview %>%
+  filter(Temp != "Day")
+
+write_csv(Activity_Overview, "~/Library/CloudStorage/Egnyte-mpgcloud/Shared/Workspace/Teams/Avian Science/Motus/Project 213 Data/Projects/COPO_Lunar Breeding Season/Data Tables/Activity_Overview Table.csv")
+
+####Visualize Overview####
+###PLOT 2###
+  #This visualizes all of the binned night activity together on the same plot
+  #You can see the variation in the bins and the affect on activity levels
+#create vector
+mfg_ids <- unique(Activity_Overview$mfgID)
+
+#Loop through vector and save each plot
+for (ID in mfg_ids) {
+  plot_data <- filter(Activity_Overview.sht, mfgID == ID)
+
+  p <- ggplot(plot_data, aes(x = Night_ID)) +
+    geom_line(aes(y = Active, color = as.factor(Bin_Length))) +
+    labs(
+      title = paste("Minutes Active per Night by Bin Length for COPO:", ID),
+      x = "Night",
+      y = "Minutes Active",
+      color = "Bin Length (mins)"
+    ) +
+    scale_color_viridis_d() +  
+    theme_bw() +
+    theme(text = element_text(family = "serif", size = 12))
+  
+  ggsave(filename = paste0("Minutes Active by Night_Overview_COPO ", ID, ".png"), 
+         path = "~/Library/CloudStorage/Egnyte-mpgcloud/Shared/Workspace/Teams/Avian Science/Motus/Project 213 Data/Projects/COPO_Lunar Breeding Season/Figures/8.28.24/Overview", 
+         width = 7, 
+         height = 5, 
+         units = "in")
+}
+
+
+####
+
+####Lunar Variables####
+
+#Load lunar variables
+  #Code out import line
+Moon.dat<- read_csv("~/Library/CloudStorage/Egnyte-mpgcloud/Shared/Workspace/Teams/Avian Science/Motus/Project 213 Data/Projects/COPO_Lunar Breeding Season/Data Tables/Moon Variables 2024.csv")
+
+#reformat POSIXCT variables!!
+
+#reduce variables and join with detection table by time_bin  
+Moon.sht<- Moon.dat %>%
+  select(time_bin, fraction, phase, angle, altitude,
+         night_mins, moon_available, moon_duration)
+
+Activity_Table.5b<- Activity_Table.5 %>% 
+  left_join(Moon.sht, by = "time_bin")
+
+write_csv(Activity_Table.5b, "~/Library/CloudStorage/Egnyte-mpgcloud/Shared/Workspace/Teams/Avian Science/Motus/Project 213 Data/Projects/COPO_Lunar Breeding Season/Data Tables/Activity_Table_5mins.csv")
+
+
+####PAST VERSIONS####
 ##Load detections##
 COPO.detect<-read_csv("./Processed Data/COPO/7.4.24/COPO_tag_detections_FILTERED_7.4.24.csv")
 
@@ -211,8 +467,6 @@ for (ID in mfg_ids) {
 ####
 
 
-
-####PAST VERSIONS####
 ##Assign Time Bins##
 COPO.act<- COPO.detect %>%
   filter(ts_LocalTime >= "2024-05-01 00:00:00") %>%
